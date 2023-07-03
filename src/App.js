4033aa16ec41cae824063bdcd7c631771d1c6a5e
juanvictorf <estudantejuan0@gmail.com>
2023-07-03 20:31:19 -0300

    subindo projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Component } from "react";
+import './estilo.css';
+import './index.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: "",
+    };
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = ['Viva o momento, aproveite as oportunidades.',
+    'Você é mais forte do que imagina.',
+    'Acredite em si mesmo e conquiste seus sonhos.',
+    'A vida é uma jornada cheia de surpresas e aprendizados.',
+    'Sua criatividade é uma fonte infinita de possibilidades.',
+    'Sorria, contagie o mundo com sua alegria.',
+    'A gratidão transforma pequenos gestos em grandes momentos.']
+  }
+
+  quebraBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = '"' + this.frases[numeroAleatorio] + '"';
+    this.setState(state);
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <h1>Biscoito da sorte</h1>
+        <img src={require('./assets/biscoito.png')} className="img" />
+        <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito}/>
+        <h3 className="textoFrase">{this.state.textoFrase}</h3>
+      </div>
+    );
+  };
+}
+
+class Botao extends Component {
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+      </div>
+    );
+  }
 }
 
 export default App;
